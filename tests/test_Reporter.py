f5e2ad1d74df9d89d36e28446f5f9663773aa2a4
MentalBlood <neceporenkostepan@gmail.com>
2021-10-23 12:41:25 +0300

    added Reporter

---
+++ tests/test_Reporter.py
@@ -0,0 +1,19 @@
+from math import sqrt
+from sharpener import Reporter
+
+
+
+def test_stats():
+
+	f = lambda n: [sqrt(i) for i in range(n)]
+
+	reporter = Reporter(10)(f)
+	report = reporter(10 ** 4)
+
+	assert type(report) == dict
+	
+	for k, v in report.items():
+		assert type(k) == str
+		assert type(v) == dict
+		assert 'number' in v
+		assert 'time' in v
--- tests/test_profile.py
+++ tests/test_profile.py
@@ -5,16 +5,16 @@ from sharpener import Profile
 
 def test_stats():
 
-    p = Profile()
-    
-    p.enable()
-    [sqrt(i) for i in range(10 ** 6)]
-    p.disable()
-    
-    assert type(p.report) == dict
-    
-    for k, v in p.report.items():
-        assert type(k) == str
-        assert type(v) == dict
-        assert 'number' in v
-        assert 'time' in v
+	p = Profile()
+	
+	p.enable()
+	[sqrt(i) for i in range(10 ** 4)]
+	p.disable()
+	
+	assert type(p.report) == dict
+	
+	for k, v in p.report.items():
+		assert type(k) == str
+		assert type(v) == dict
+		assert 'number' in v
+		assert 'time' in v
