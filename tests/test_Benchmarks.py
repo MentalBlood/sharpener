f534137aa27eaebf7e3ff9b99df41c5baa1bda3e
necheporenko_s_iu <neceporenkostepan@gmail.com>
2021-10-26 17:39:14 +0300

    fixed tests according to config and benchmarks

--- tests/test_Benchmarks.py
+++ tests/test_Benchmarks.py
@@ -11,8 +11,8 @@ def test_init():
 
 	assert sorted(b.modules.keys()) == ['list', 'math']
 
-	assert sorted(b.modules['list'].keys()) == ['index']
-	assert sorted(b.modules['math'].keys()) == ['power', 'sqr', 'sqrt']
+	assert sorted(b.modules['list'].keys()) == ['Index']
+	assert sorted(b.modules['math'].keys()) == ['Power', 'Sqr', 'Sqrt']
 
 
 def test_run():
@@ -20,10 +20,10 @@ def test_run():
 	b = Benchmarks()
 
 	reports = b.run({
-		'math::sqrt': {
+		'math::Sqrt': {
 			'n': 10 ** 5
 		},
-		'list::index': {
+		'list::Index': {
 			'l': [i for i in range(10 ** 6)],
 			'e': 2
 		}
@@ -31,6 +31,6 @@ def test_run():
 
 	assert type(reports) == Reports
 	assert len(reports) == 2
-	assert 'time' in reports['math::sqrt']
-	assert 'calls' in reports['math::sqrt']
-	assert not any([c in reports['math::sqrt']['calls'] for c in ["<method 'disable' of '_lsprof.Profiler' objects>"]])
+	assert 'time' in reports['math::Sqrt']
+	assert 'calls' in reports['math::Sqrt']
+	assert not any([c in reports['math::Sqrt']['calls'] for c in ["<method 'disable' of '_lsprof.Profiler' objects>"]])
